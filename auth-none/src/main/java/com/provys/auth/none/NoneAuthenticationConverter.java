4e26b67bfeb1a8e50d321bc38ed9da7d0241c077
MichalStehlikCz <michal.stehlik.cz@gmail.com>
2020-06-08 10:39:06 +0200

    None converter can be singleton

--- auth-none/src/main/java/com/provys/auth/none/NoneAuthenticationConverter.java
+++ auth-none/src/main/java/com/provys/auth/none/NoneAuthenticationConverter.java
@@ -8,9 +8,16 @@ import org.springframework.security.web.authentication.AuthenticationConverter;
  * Converter that converts any request into NoneAuthenticationToken. Used to allow inclusion of
  * NoneAuthProvider without having any information in request.
  */
-public class NoneAuthenticationConverter implements AuthenticationConverter {
+public final class NoneAuthenticationConverter implements AuthenticationConverter {
 
   private static final NoneAuthenticationConverter INSTANCE = new NoneAuthenticationConverter();
+
+  public static NoneAuthenticationConverter getInstance() {
+    return INSTANCE;
+  }
+
+  private NoneAuthenticationConverter() {
+  }
 
   @Override
   public Authentication convert(HttpServletRequest httpServletRequest) {
